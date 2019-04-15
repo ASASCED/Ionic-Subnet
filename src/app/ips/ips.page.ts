bab6ae7deed90c0ced94229be49c3b68ee849660
Jesus Jimenez Cordero <jesus_heaven117@outlook.com>
2019-04-15 03:11:53 -0500

    Funcionalidad de añadido de HOSTS

--- src/app/ips/ips.page.ts
+++ src/app/ips/ips.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { dashCaseToCamelCase } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-ips',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IpsPage implements OnInit {
 
+  hosts: string[] = [];
+  count: number = 0;
+  finalHosts: number[] = [];
+  numero: number = 0;
+  HostJSON: any[] = [
+    {
+      ip: '170.0.4.2',
+      hosts: this.hosts
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addHost() {
+    this.hosts.push(`input${this.numero++}`);
+    console.log(this.hosts);
+  }
+
+  calcularVLSM() {
+    let dato: any = document.getElementsByClassName('hostCount');
+    for (let i = 0; i < this.hosts.length; i++) {
+      this.finalHosts.push(dato[i].value);
+    }
+    console.log(this.finalHosts);
   }
 
 }
